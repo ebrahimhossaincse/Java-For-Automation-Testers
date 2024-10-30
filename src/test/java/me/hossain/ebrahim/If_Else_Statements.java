3211fb975fbf83dcb1a10e4670f9608301d4fead
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-10-30 07:17:47 -0400

    add condition examples

---
+++ src/test/java/me/hossain/ebrahim/If_Else_Statements.java
@@ -0,0 +1,17 @@
+package me.hossain.ebrahim;
+
+public class If_Else_Statements {
+
+	public static void main(String[] args) {
+		// Defining a variable 'marks' to represent a student's score
+		int marks = 75;
+
+		// Using an if-else statement to check if the student has passed or failed
+		if (marks >= 50) {                    // Condition: if 'marks' are 50 or above
+		    System.out.println("Pass");        
+		} else {
+		    System.out.println("Fail");       
+		}
+	}
+
+}
--- src/test/java/me/hossain/ebrahim/RelationalOperators.java
+++ src/test/java/me/hossain/ebrahim/RelationalOperators.java
@@ -3,7 +3,23 @@ package me.hossain.ebrahim;
 public class RelationalOperators {
 
 	public static void main(String[] args) {
-		
+		// Initializing two integer variables, 'x' and 'y'
+		int x = 8;
+		int y = 12;
+
+		// Using relational operators to compare 'x' and 'y'
+
+		boolean isEqual = (x == y);       // Equal to: Checks if 'x' is equal to 'y' (8 == 12) => false
+		boolean isNotEqual = (x != y);    // Not equal to: Checks if 'x' is not equal to 'y' (8 != 12) => true
+		boolean isGreater = (x > y);      // Greater than: Checks if 'x' is greater than 'y' (8 > 12) => false
+		boolean isLesser = (x < y);       // Less than: Checks if 'x' is less than 'y' (8 < 12) => true
+
+		// Displaying the results of each comparison
+		System.out.println("Is Equal: " + isEqual);  
+		System.out.println("Is Not Equal: " + isNotEqual);
+		System.out.println("Is Greater: " + isGreater);   
+		System.out.println("Is Lesser: " + isLesser);   
+
 	}
 
 }
